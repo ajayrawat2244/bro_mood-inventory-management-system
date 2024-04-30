6c76a4b9c11f01dd09423663aec49e67202e7bbe
Ravinder Singh <ssonukr93@gmail.com>
2024-04-30 07:28:17 +0530

    initial commit

--- bro_mood_inventory_system/bro_mood_inventory_system/urls.py
+++ bro_mood_inventory_system/bro_mood_inventory_system/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('outlet/', admin.site.urls),
     path('outlet/', include('outlet.urls')),
+    # path('product/', include('product.urls')),
     path('', include('home.urls')),
 ]
--- bro_mood_inventory_system/outlet/models.py
+++ bro_mood_inventory_system/outlet/models.py
@@ -1,9 +1,9 @@
 # from django.db import models
 #
 # class Outlet(models.Model):
-#     name = models.CharField(max_length=100)
+#     name = models.CharField(max_length=1000)
 #     address = models.TextField()
-#     contact_information = models.CharField(max_length=100)
+#     contact_information = models.CharField(max_length=1000)
 #
 # class OutletInventory(models.Model):
 #     outlet = models.ForeignKey('Outlet', on_delete=models.CASCADE)
@@ -17,7 +17,7 @@
 #     movement_type = models.CharField(max_length=3, choices=[('IN', 'In'), ('OUT', 'Out')])
 #     quantity = models.IntegerField()
 #     date = models.DateField()
-#     reference = models.CharField(max_length=100)
+#     reference = models.CharField(max_length=1000)
 #
 #
 # class OutletTotalStock(models.Model):
