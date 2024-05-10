87f70648f535fd18dda05745d16491a3b7542584
Ravinder Singh <ssonukr93@gmail.com>
2024-05-10 17:37:23 +0530

    init

--- bro_mood_inventory_system/home/urls.py
+++ bro_mood_inventory_system/home/urls.py
@@ -1,8 +1,10 @@
 # urls.py
 from django.urls import path
-from .views import item_list_view
+from .views import item_list_view, login, register
 
 urlpatterns = [
     path('', item_list_view, name='item-list'),
+    path('login', login, name='login'),
+    path('register', register, name='register'),
     # other URL patterns...
 ]
--- bro_mood_inventory_system/home/views.py
+++ bro_mood_inventory_system/home/views.py
@@ -1,8 +1,16 @@
 # views.py
 from django.shortcuts import render
 
+
 def item_list_view(request):
     return render(request, 'index.html')
 
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def register(request):
+    return render(request, 'register.html')
 
 # Create your views here.
