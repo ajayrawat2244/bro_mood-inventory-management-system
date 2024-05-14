93db82d062ad228310b7137f9f2b671b2827c542
Ajay Singh Rawat <ajrawat2244@gmail.com>
2024-05-14 09:51:35 +0530

    list & add productVariant

--- bro_mood_inventory_system/product/models.py
+++ bro_mood_inventory_system/product/models.py
@@ -8,7 +8,8 @@ class Product(models.Model):
     base_price = models.DecimalField(max_digits=10, decimal_places=2)
     reorder_level = models.PositiveIntegerField(default=0)
     supplier = models.ForeignKey('Supplier', on_delete=models.CASCADE)
-
+    def __str__(self):
+        return (self.name)
 
 class Supplier(models.Model):
     name = models.CharField(max_length=100)
--- bro_mood_inventory_system/product/urls.py
+++ bro_mood_inventory_system/product/urls.py
@@ -6,7 +6,10 @@ urlpatterns = [
     path('', item_list_view, name='item-list'),
     path('add-supplier', add_supplier, name='add-supplier'),
     path('add-product', add_product, name='add-product'),
-    path('list', product_list, name='product-list')
+    path('list', product_list, name='product-list'),
+    path('supplier-list', supplier_list, name='supplier-list'),
+    path('add-productVariant', add_productVariant, name='add-productVariant'),
+    path('productVariant-list', productVariant_list, name='productVariant-list')
 
     # other URL patterns...
 ]
