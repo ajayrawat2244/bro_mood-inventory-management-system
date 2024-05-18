a2565a345cab7e855a8bea9bf32035f95fc996a0
Ravinder Singh <ssonukr93@gmail.com>
2024-05-18 17:01:36 +0530

    init

--- bro_mood_inventory_system/product/models.py
+++ bro_mood_inventory_system/product/models.py
@@ -6,9 +6,9 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     category = models.CharField(max_length=50)
-    base_price = models.DecimalField(max_digits=10, decimal_places=2)
+    # base_price = models.DecimalField(max_digits=10, decimal_places=2)
     reorder_level = models.PositiveIntegerField(default=0)
-    supplier = models.ForeignKey('Supplier', on_delete=models.CASCADE)
+    # supplier = models.ForeignKey('Supplier', on_delete=models.CASCADE)
     brand = models.CharField(max_length=100, null=True)
 
     def __str__(self):
@@ -26,6 +26,7 @@ class Supplier(models.Model):
     def __str__(self):
         return self.name
 
+
 # class ProductSize(models.Model):
 #     product = models.ForeignKey(Product, on_delete=models.CASCADE)
 #     size = models.ForeignKey(Size, on_delete=models.CASCADE)
@@ -42,4 +43,4 @@ class ProductVariant(models.Model):
     supplier_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, default=0)
     selling_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, default=0)
     customer_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, default=0)
-
+    supplier = models.ForeignKey('Supplier', on_delete=models.CASCADE, null=True)
